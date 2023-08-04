5934d6be7eff215f9d2db48f7416685067f2005d
Kmart002 <128054511+Kmart002@users.noreply.github.com>
2023-08-04 15:01:55 +0100

    Add files via upload

---
+++ components/table.js
@@ -0,0 +1,28 @@
+const Table = () => {
+  return (
+    <div className="table-wrapper">
+      <table className="fl-table">
+        <thead>
+          <tr>
+            <th>Header 1</th>
+            <th>Header 2</th>
+            <th>Head er 3</th>
+            <th>Header 4</th>
+            <th>Header 5</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Content 1</td>
+            <td>Content 1</td>
+            <td>Content 1</td>
+            <td>Content 1</td>
+            <td>Content 1</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
